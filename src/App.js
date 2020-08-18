f5146495646bc69b56274490d20b2aa66efabac2
Aaron <aacheng17@gmail.com>
2020-08-18 18:59:34 -0400

    Use navList inside of routes
    -This is to allow for accessing current route in the navList

--- src/App.js
+++ src/App.js
@@ -10,40 +10,39 @@ import Dots from "./components/Dots.js"
 import Page from "./pages/Page.js"
 import pages from "./Pages.js"
 
-var routes = [], navList = [];
+var navList = [];
+for (var i=0;i<pages.length;i++) {
+  var page = pages[i];
+  navList.push(
+    <li key={`navList${i}`}>
+      <Link to={page.path} style={{}}>{page.navLink}</Link>
+    </li>
+  );
+}
+
+var routes = [];
 for (var i=0;i<pages.length;i++) {
   var page = pages[i];
   routes.push(
     <Route exact path={page.path} key={`route${i}`}>
+      <ul className="Nav-list">
+        {navList}
+      </ul>
+      <hr />
       <Page slides={page.slides} />
     </Route>
-  );
-  navList.push(
-    <li key={`navList${i}`}>
-      <Link to={page.path}>{page.navLink}</Link>
-    </li>
   );
 }
 
 export default function App() {
   return (
     <div className="div-app">
-      <div>
-        <Dots />
-      </div>
-      <div>
-        <Router>
-          <ul className="Nav-list">
-            {navList}
-          </ul>
-          
-          <hr />
-
-          <Switch>
-            {routes}
-          </Switch>
-        </Router>
-      </div>
+      <Dots />
+      <Router>
+        <Switch>
+          {routes}
+        </Switch>
+      </Router>
     </div>
   );
 }
