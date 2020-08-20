514613e058a5697bd22d830d0451e1f76b926d8f
Aaron <aacheng17@gmail.com>
2020-08-20 14:05:15 -0400

    Add icims internship to work page

--- src/Pages.js
+++ src/Pages.js
@@ -57,6 +57,8 @@ const pages = [
           </div>
           <h2>Founder and Tutor of <a href="https://tutors.com/nj/sayreville/math-tutors/aaron-cheng-tutoring">A* Tutoring</a></h2>
           <p>2013 - Present<br />Private tutoring for programming, math, physics, music, and English</p>
+          <h2>Intern at iCIMS</h2>
+          <p>2020<br />Mentored by a Senior Developer and worked with fellow mentees to build a full stack web application.</p>
           <h2>Math Tutor at Mathnasium Learning Center</h2>
           <p>2019 - Present<br />K-12 math tutor</p>
           <h2>Freelance Musician</h2>
