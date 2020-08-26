6e9ebe1f7bd5366b1470be3819dd78dd1fc8e90f
Aaron <aacheng17@gmail.com>
2020-08-26 18:22:24 -0400

    Sparkles Off button now disappeares

--- src/components/SparklesButton.js
+++ src/components/SparklesButton.js
@@ -3,10 +3,14 @@ import React, { Component } from "react";
 export default class SparklesButton extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      sparkles: false
+    }
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(event, onOff) {
+    this.setState({ sparkles: onOff });
     this.props.onClick(onOff);
   }
 
@@ -16,9 +20,12 @@ export default class SparklesButton extends Component {
         <button className='Nav-link' onClick={(event) => this.handleClick(event, true)}>
           sparkles
         </button>
-        <button className='Nav-link' onClick={(event) => this.handleClick(event, false)}>
-          off
-        </button>
+        {
+          this.state.sparkles ? 
+          <button className='Nav-link' onClick={(event) => this.handleClick(event, false)}>
+            off
+          </button> : ""
+        }
       </div>
     );
   }
