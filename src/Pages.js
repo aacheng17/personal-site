3665853e3d7000ba6a9f30afba75168743480fd5
Aaron <aacheng17@gmail.com>
2020-08-28 15:22:21 -0400

    Links open in new tab

--- src/Pages.js
+++ src/Pages.js
@@ -34,17 +34,17 @@ const pages = [
             <br />
           </h3>
           <p>-</p>
-          <a href="https://github.com/aacheng17">GitHub</a>
+          <a href="https://github.com/aacheng17" target="_blank" rel="noopener noreferrer">GitHub</a>
           <br/>
-          <a href="https://www.linkedin.com/in/aaron-cheng-b93a711a7/">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/aaron-cheng-b93a711a7/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           <br/>
-          <a href="https://app.joinhandshake.com/users/9912209">HandShake</a>
+          <a href="https://app.joinhandshake.com/users/9912209" target="_blank" rel="noopener noreferrer">HandShake</a>
           <br/>
-          <a href="https://aacheng.itch.io">itch.io</a>
+          <a href="https://aacheng.itch.io" target="_blank" rel="noopener noreferrer">itch.io</a>
           <br/>
-          <a href="https://tutors.com/nj/sayreville/math-tutors/aaron-cheng-tutoring">A* Tutoring</a>
+          <a href="https://tutors.com/nj/sayreville/math-tutors/aaron-cheng-tutoring" target="_blank" rel="noopener noreferrer">A* Tutoring</a>
           <br/>
-          <a href="http://aaronchengmusic.com">Music</a>
+          <a href="http://aaronchengmusic.com" target="_blank" rel="noopener noreferrer">Music</a>
 
         </div>
       )
@@ -60,13 +60,13 @@ const pages = [
           <div style={{ textAlign: "center" }}>
             <h1>Work</h1>
           </div>
-          <h2>Founder and Tutor of <a href="https://tutors.com/nj/sayreville/math-tutors/aaron-cheng-tutoring">A* Tutoring</a></h2>
+          <h2>Founder and Tutor of <a href="https://tutors.com/nj/sayreville/math-tutors/aaron-cheng-tutoring" target="_blank" rel="noopener noreferrer">A* Tutoring</a></h2>
           <p>2013 - Present<br />Private tutoring for programming, math, physics, music, and English</p>
           <h2>Intern at iCIMS</h2>
           <p>2020<br />Mentored by a senior software engineer and worked with fellow mentees to build a full stack web application.</p>
           <h2>Math Tutor at Mathnasium Learning Center</h2>
           <p>2019 - Present<br />K-12 math tutor</p>
-          <h2><a href="https://aaronchengmusic.com">Freelance Musician</a></h2>
+          <h2><a href="https://aaronchengmusic.com" target="_blank" rel="noopener noreferrer">Freelance Musician</a></h2>
           <p>2017 - Present<br />Classical, jazz, and pop pianist for local events and weddings</p>
           <h2>Music and Tech Intern for the Rockit! Live Foundation</h2>
           <p>2018 - 2019<br />Editing and design for website, social media, flyers, photos, videos</p>
@@ -158,46 +158,46 @@ const pages = [
           <p>
             Website you're currently on<br/>
             Javascript<br/>
-            <a href="https://github.com/aacheng17/personal-site">GitHub</a>
+            <a href="https://github.com/aacheng17/personal-site" target="_blank" rel="noopener noreferrer">GitHub</a>
           </p>
           <h2>Ratings Site</h2>
           <p>
             Website for aggregated movie ratings<br/>
             Javascript, Java, Python<br/>
-            <a href="https://ratingssite.azurewebsites.net">Link to site</a><br/>
-            <a href="https://github.com/aacheng17/RatingsSite">GitHub</a>
+            <a href="https://ratingssite.azurewebsites.net" target="_blank" rel="noopener noreferrer">Link to site</a><br/>
+            <a href="https://github.com/aacheng17/RatingsSite" target="_blank" rel="noopener noreferrer">GitHub</a>
           </p>
           <h2>BadTranslator</h2>
           <p>
             Website for translating things badly<br/>
             Dart, Javascript<br/>
-            <a href="https://badtranslator-284415.web.app">Link to site</a><br/>
-            <a href="https://github.com/aacheng17/BadTranslator">GitHub</a>
+            <a href="https://badtranslator-284415.web.app" target="_blank" rel="noopener noreferrer">Link to site</a><br/>
+            <a href="https://github.com/aacheng17/BadTranslator" target="_blank" rel="noopener noreferrer">GitHub</a>
           </p>
           <h2>Steal Shield Stab</h2>
           <p>
             Game about fighting and bluffing friends<br/>
             GameMaker Language<br/>
-            <a href="https://aacheng.itch.io/stealshieldstab">itch.io</a>
+            <a href="https://aacheng.itch.io/stealshieldstab" target="_blank" rel="noopener noreferrer">itch.io</a>
           </p>
           <h2>TaxiGame</h2>
           <p>
             Game about zipping through streets as a taxi<br/>
             Java<br/>
-            <a href="https://aacheng.itch.io/taxigame">itch.io</a><br/>
-            <a href="https://github.com/RyanCirincione/TaxiGame">GitHub</a>
+            <a href="https://aacheng.itch.io/taxigame" target="_blank" rel="noopener noreferrer">itch.io</a><br/>
+            <a href="https://github.com/RyanCirincione/TaxiGame" target="_blank" rel="noopener noreferrer">GitHub</a>
           </p>
           <h2>The Cactus</h2>
           <p>
             Game about surviving a cactus<br/>
             GameMaker Language<br/>
-            <a href="https://aacheng.itch.io/the-cactus">itch.io</a>
+            <a href="https://aacheng.itch.io/the-cactus" target="_blank" rel="noopener noreferrer">itch.io</a>
           </p>
           <h2>Morse</h2>
           <p>
             Game about clicking at the right time<br/>
             GameMaker Language<br/>
-            <a href="https://aacheng.itch.io/morse">itch.io</a>
+            <a href="https://aacheng.itch.io/morse" target="_blank" rel="noopener noreferrer">itch.io</a>
           </p>
         </div>
       )
