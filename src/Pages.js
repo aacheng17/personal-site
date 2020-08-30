4256d7f028ff078cb076ee6a9800a12a7a52f3f6
Aaron <aacheng17@gmail.com>
2020-08-29 23:58:24 -0400

    Big content changes to Pages.js

--- src/Pages.js
+++ src/Pages.js
@@ -61,33 +61,70 @@ const pages = [
             <h1>Work</h1>
           </div>
           <h2>Intern at iCIMS</h2>
-          <p>2020 - Present<br />
-          • Mentored directly by a senior software engineer at iCIMS<br/>
-          • Collaborated with intern team to build and deploy a full stack web application (see Ratings Site under Projects)<br/>
-          • Gain experience in web development, front end web design, web scraping, databases, web APIs, and collaboration using source control
+          <p>2020 - Present<br /><br/>
+          I got the amazing opportunity to be an intern at iCIMS, a group of the friendliest, smartest, and most considerate professionals I know. I worked one on one with my mentor, a senior software engineer at the company, and also with two other interns to build my first full stack web application (It's called "Ratings Site" in my Projects).
+          <br/><br/>Things I've learned:
+          <br/>• Web development with Java Spring
+          <br/>• Front end web design with ReactJS (which is what led to me making this website)
+          <br/>• Web scraping with Python and the Beautiful Soup module
+          <br/>• Databases and SQL
+          <br/>• Using web APIs
+          <br/>• Collaboration and source control using GitHub
           </p>
+          <br/>
+
           <h2>Founder and Tutor of <a href="https://tutors.com/nj/sayreville/math-tutors/aaron-cheng-tutoring" target="_blank" rel="noopener noreferrer">A* Tutoring</a></h2>
-          <p>2013 - Present<br />
-          • Teach programming, math, physics, music, and English with a focus on intuitive understanding<br/>
-          • Manage communications, scheduling, and payments with students and parents<br/>
-          • Adapt teaching style to accommodate a wide range of students with different ages, experience levels, skillsets, backgrounds, dispositions, and goals<br/>
-          • Conduct marketing and corresponding with potential students for myself as well as two other A* tutors
+          <p>2013 - Present<br /><br/>
+          I've been teaching since I was a freshman in high school. It started with piano but in the past few years I've expanded to programming, K-12 math, AP Physics, and English. Since I started two other teachers have begun teaching under the name of A* Tutoring, covering chemistry, multivariable calculus, Spanish, violin, viola, and ESL. I've acted as the marketer, communicating with potential students. I pride myself on working towards making material intuitive to students, not just aiming for a grade, and adapting to different student goals and needs.
+          <br/><br/>Things I've learned:
+          <br/>• Teaching and communicating skills: Breaking an idea down and presenting it clearly
+          <br/>• Interpersonal skills: Understanding what others are thinking and why they are struggling or succeeding
+          <br/>• Managing: Organizing schedules and payments, as well as coordinating with other teachers
+          <br/>• Marketing: Presenting my service honestly, effectively, and across different mediums
           </p>
+          <br/>
+
           <h2>Math Tutor at Mathnasium Learning Center</h2>
-          <p>2019 - Present<br />
-          • Trained in specialized method to enable and encourage children to understand and appreciate math<br/>
-          • Engage with local K-12 students to teach in-person and online group math lessons
+          <p>2019 - Present<br /><br/>
+          Mathnasium approaches teaching math with the goal of encouraging children to not only understand but also appreciate and be confident in math. They have personalized learning plans for each student and clear methods for instructors to break down and teach concepts, in which they trained me. I really admire their goal and I'm glad to be working with my center director and teaching math to local kids at Mathnasium.
+          <br/><br/>Things I've learned:
+          <br/>• Relearning math in its basic concepts in a way that an absolute beginner can understand
+          <br/>• Trying different strategies of articulating these concepts 
+          <br/>• Engaging with kids of all different personalities and learning needs
           </p>
+          <br/>
+
           <h2><a href="https://aaronchengmusic.com" target="_blank" rel="noopener noreferrer">Freelance Musician</a></h2>
-          <p>2017 - Present<br />
-          • Perform classical, jazz, and pop piano with other musicians for local events</p>
-          <h2>Music and Tech Intern for the Rockit! Live Foundation</h2>
-          <p>2018 - 2019<br />
-          • Edited photos and videos for website and social media<br/>
-          • Designed posters and flyers for events and advertising
+          <p>2017 - Present<br /><br/>
+          In high school I started playing jazz at local events with some friends and since then I've performed at restaurants, weddings, and coffeehouses. I absolutely love music and I'm mad I don't get the chance to play as often now. I really enjoy improvising and I would love to one day release music or develop an AI that improvises music.
+          <br/><br/>Things I've learned:
+          <br/>• Music: Improvisation, arranging, group performance, sound engineering
+          <br/>• Confidence in front of a crowd
+          <br/>• Making the best of mistakes
+          <br/>• How to practice
+          <br/>• Working with a team and sticking to a plan
           </p>
+          <br/>
+
+          <h2>Music and Tech Intern for the Rockit! Live Foundation</h2>
+          <p>2018 - 2019<br /><br/>
+          Rockit is a nonprofit music education and performance program headed by an extremely passionate and driven group of musicians, possibly only matched by the talent and passion of the kids they teach. I worked closely with the founder and head of the organization, putting together videos, editing photos for the website and postcards, assembling ad journals, designing posters, flyers, and event invitations, and corresponding with printing companies. They're an amazing educational program and I'm proud to have been a part of it, those kids are insane musicians.
+          <br/><br/>Things I learned:
+          <br/>• Photoshop and Inkscape Vector Graphics
+          <br/>• Visual design and the printing and marketing process
+          <br/>• Turning a design into a professional finished product
+          </p>
+          <br/>
+
           <h2>Video Production Intern at Cinecall Productions</h2>
-          <p>2017<br />• Video editing for concerts and documentaries</p>
+          <p>2017<br /><br/>
+          Cinecall is a great little studio that does video post production and I had the privilege of working directly with the head of it. I sorted through footage and cut together video of things ranging from a documentary on an organization that provided free cruises to cancer patients to concert DVDs.
+          <br/><br/>Things I learned:
+          <br/>• Adobe Premiere
+          <br/>• Video editing and production techniques
+          <br/>• General knowledge of professional video recording and production
+          </p>
+          <br/>
         </div>
       ),
       new Slide(
@@ -110,6 +147,7 @@ const pages = [
             NCH Videopad Editor, Inkscape Vector Graphics<br />
             Blender, Autodesk Inventor<br/>
           </p>
+          <br/>
         </div>
       ),
       new Slide(
@@ -155,6 +193,7 @@ const pages = [
             <li>Multimedia Composition</li>
             <li>Information Technology and Informatics</li>
           </ul>
+          <br/>
         </div>
       )
     ],
@@ -168,49 +207,43 @@ const pages = [
         <div>
           <h2>Personal Site</h2>
           <p>
-            Website you're currently on<br/>
-            Javascript<br/>
+            This is the website you're currently on! Just a little project to practice using ReactJS. Building the fade animation and the sparkles from scratch probably took way too long.<br/>
             <a href="https://github.com/aacheng17/personal-site" target="_blank" rel="noopener noreferrer">GitHub</a>
           </p>
           <h2>Ratings Site</h2>
           <p>
-            Website for aggregated movie ratings<br/>
-            Javascript, Java, Python<br/>
+            A full stack web application that scrapes the top movies from IMDb, Rotten Tomatoes, and MetaCritic and aggregates the data in one website, developed with two other team members. We built the backend with Java Spring, the front end with ReactJS, web scraped with Python, and hosted the database and deployed the site with Microsoft Azure. It was very educational to work on all aspects of a full stack app.<br/>
             <a href="https://ratingssite.azurewebsites.net" target="_blank" rel="noopener noreferrer">Link to site</a><br/>
             <a href="https://github.com/aacheng17/RatingsSite" target="_blank" rel="noopener noreferrer">GitHub</a>
           </p>
           <h2>BadTranslator</h2>
           <p>
-            Website for translating things badly<br/>
-            Dart, Javascript<br/>
+            A little project I made for fun, consisting of a front end made with Flutter that takes your input and queries a backend built with NodeJS. The backend is an API that uses the Google Translate API. It translates your input through 10 random languages and then back to English for a likely terrible translation. Only works for English right now.<br/>
             <a href="https://badtranslator-284415.web.app" target="_blank" rel="noopener noreferrer">Link to site</a><br/>
             <a href="https://github.com/aacheng17/BadTranslator" target="_blank" rel="noopener noreferrer">GitHub</a>
           </p>
           <h2>Steal Shield Stab</h2>
           <p>
-            Game about fighting and bluffing friends<br/>
-            GameMaker Language<br/>
+            Built in GameMaker Studio, this is the most complicated game I've made. Short explanation is it's rock paper scissors combined with a 2D fighting game. It hasn't gotten any intensive testing at all but I'm happy I made it.<br/>
             <a href="https://aacheng.itch.io/stealshieldstab" target="_blank" rel="noopener noreferrer">itch.io</a>
           </p>
           <h2>TaxiGame</h2>
           <p>
-            Game about zipping through streets as a taxi<br/>
-            Java<br/>
+            A very calm game made in Java Swing, collaborating with a team member. You're a dot on a track and you zip around collecting customers, money, tracks, and upgrades. A lot of focus was put on the simple geometric graphics, it's supposed to be just a neat little pleasing time killer.<br/>
             <a href="https://aacheng.itch.io/taxigame" target="_blank" rel="noopener noreferrer">itch.io</a><br/>
             <a href="https://github.com/RyanCirincione/TaxiGame" target="_blank" rel="noopener noreferrer">GitHub</a>
           </p>
           <h2>The Cactus</h2>
           <p>
-            Game about surviving a cactus<br/>
-            GameMaker Language<br/>
+            You're a green square and enemies are trying to bump you into a cactus. I would love to take this idea and run harder with it eventually. Made in GameMaker Studio<br/>
             <a href="https://aacheng.itch.io/the-cactus" target="_blank" rel="noopener noreferrer">itch.io</a>
           </p>
           <h2>Morse</h2>
           <p>
-            Game about clicking at the right time<br/>
-            GameMaker Language<br/>
+            Minimalistic game about clicking on rectangles at the right time. My goal was to make something simple, clean, and finished. Made in GameMaker Studio<br/>
             <a href="https://aacheng.itch.io/morse" target="_blank" rel="noopener noreferrer">itch.io</a>
           </p>
+          <br/>
         </div>
       )
     ],
