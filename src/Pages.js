97a4adaab2bb80d69f989cff480012c247e52237
Aaron <aacheng17@gmail.com>
2022-02-01 14:17:36 -0800

    Update pages

--- src/Pages.js
+++ src/Pages.js
@@ -40,13 +40,6 @@ const pages = [
           <a href="https://github.com/aacheng17" target="_blank" rel="noopener noreferrer">GitHub</a>
           <br/>
           <a href="https://aacheng.itch.io" target="_blank" rel="noopener noreferrer">itch.io</a>
-          <br/><br/><br/>
-          <b>A bit about me</b>
-          <br/>
-          Things that I hold close to my heart include philosophy of science, electronic jazz, and 2D art and animation.
-          <br/><br/>Right now I'm working on mindfulness, rock climbing, and video game skills.
-          <br/><br/>I love dry and abrupt humor.
-          <br/><br/>I've been having a lot of fun teaching CS but I think my favorite teaching experience is the semester where I helped a professional cook pass their online physics and cooking class.
         </div>
       )
     ]
@@ -64,74 +57,51 @@ const pages = [
 
           <h2>Software Development Engineer at Amazon Web Services</h2>
           <p>2021-Present<br /><br/>
-          Java and C#
+          • Develop in Java and C# to maintain and grow SQL Server high-availability database hosting services
+          <br/>• Collaborate with teammates to design and write high-quality, readable, and testable code
+          <br/>• Identify and make improvements to existing code, reducing tech debt and increasing code efficiency
+          <br/>• Iterate quickly, manage deployments, and own projects and metrics
+          <br/>• Perform operations work and on-call shifts to directly diagnose and fix customer facing issues
           </p>
           <br/>
 
           <h2>Founder and Tutor of <a href="https://tutors.com/nj/sayreville/math-tutors/aaron-cheng-tutoring" target="_blank" rel="noopener noreferrer">A* Tutoring</a></h2>
           <p>2013 - Present<br /><br/>
-          I've been teaching since I was a freshman in high school. It started with piano but in the past few years I've expanded to programming, K-12 math, AP Physics, and English. Since I started two other teachers have begun teaching under the name of A* Tutoring, covering chemistry, multivariable calculus, Spanish, violin, viola, and ESL. I've acted as the marketer, communicating with potential students. I pride myself on working towards making material intuitive to students, not just aiming for a grade, and adapting to different student goals and needs.
-          <br/><br/>Things I've learned:
-          <br/>• Teaching and communicating skills: Breaking an idea down and presenting it clearly
-          <br/>• Interpersonal skills: Understanding what others are thinking and why they are struggling or succeeding
-          <br/>• Managing: Organizing schedules and payments, as well as coordinating with other teachers
-          <br/>• Marketing: Presenting my service honestly, effectively, and across different mediums
+          <br/>• Teach computer science, math, physics, music, and English with a focus on intuitive understanding
+          <br/>• Manage communications, scheduling, and payments with students and parents
+          <br/>• Adapt teaching style to accommodate a wide range of students with different ages, experience levels, skillsets, backgrounds, dispositions, and goals
+          <br/>• Conduct marketing and correspondence with potential students for other tutors
           </p>
           <br/>
 
           <h2>Freelance Musician</h2>
           <p>2017 - Present<br /><br/>
-          In high school I started playing jazz at local events with some friends and since then I've performed at restaurants, weddings, and coffeehouses. I absolutely love music and I'm mad I don't get the chance to play as often now. I really enjoy improvising and I would love to one day release music or develop an AI that improvises music.
-          <br/><br/>Things I've learned:
-          <br/>• Music: Improvisation, arranging, group performance, sound engineering
-          <br/>• Confidence in front of a crowd
-          <br/>• Making the best of mistakes
-          <br/>• How to practice
-          <br/>• Working with a team and sticking to a plan
+          • Perform classical, jazz, and pop music at local events and weddings
           </p>
           <br/>
 
           <h2>Intern at iCIMS</h2>
           <p>2020<br /><br/>
-          iCIMS develops talent acquisition software that employers use to facilitate hiring processes. I worked one on one with my mentor and other interns to build a full stack web application ("Ratings Site" in my Projects).
-          <br/><br/>Things I learned:
-          <br/>• Web development with Java Spring
-          <br/>• Front end web design with ReactJS
-          <br/>• Web scraping with Python and the Beautiful Soup module
-          <br/>• Databases and SQL
-          <br/>• Using web APIs
-          <br/>• Collaboration and source control using GitHub
+          • Collaborated with intern team to build and deploy a full stack web application
+          • Gained experience in web development, front end design, MVC architecture, web scraping, web APIs
           </p>
           <br/>
 
           <h2>Math Tutor at Mathnasium Learning Center</h2>
           <p>2019 - 2021<br /><br/>
-          Mathnasium approaches teaching math with the goal of encouraging children to not only understand but also appreciate and be confident in math. I was trained in their unique teaching approach, and taught small groups of students at a time.
-          <br/><br/>Things I've learned:
-          <br/>• Relearning math in its basic concepts in a way that an absolute beginner can understand
-          <br/>• Trying different strategies of articulating these concepts 
-          <br/>• Engaging with kids of all different personalities and learning needs
+          • Taught small groups of K-12 math students
           </p>
           <br/>
 
           <h2>Music and Tech Intern for the Rockit! Live Foundation</h2>
           <p>2018 - 2019<br /><br/>
-          Rockit is an amazing nonprofit music education and performance program for kids in New Jersey. I worked closely with the founder and head of the organization, Bruce Gallipani.
-          I put videos together, edited photos for the website and postcards, assembled ad journals, designed posters, flyers, and event invitations, and corresponded with printing companies.
-          <br/><br/>Things I learned:
-          <br/>• Photoshop and Inkscape Vector Graphics
-          <br/>• Visual design and the printing and marketing process
-          <br/>• Turning a design into a professional finished product
+          • Managed website, social media, posters, flyers, and advertising
           </p>
           <br/>
 
           <h2>Video Production Intern at Cinecall Productions</h2>
           <p>2017<br /><br/>
-          Cinecall is a great production studio that often works with local charities and nonprofits. I had the privilege of working directly with the head of it, George McMorrow. I helped put together media ranging from documentaries concert DVDs.
-          <br/><br/>Things I learned:
-          <br/>• Adobe Premiere
-          <br/>• Video editing and production techniques
-          <br/>• General knowledge of professional video recording and production
+          • Produced documentary and concert videos
           </p>
           <br/>
         </div>
@@ -146,8 +116,6 @@ const pages = [
           <p>Java, C#, Python, SQL, Git</p>
           <h2>Proficient</h2>
           <p>C, JavaScript, HTML, CSS, Go</p>
-          <h2>Familiar</h2>
-          <p>PHP, Haskell, Prolog</p>
           <h2>Other Software</h2>
           <p>
             Gamemaker Studio, Unity<br />
